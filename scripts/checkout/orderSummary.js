e04b56aea8acc8b1876ffcc4d697eeac0a725c4b
Vamshi-Krishna-Musham <mushamvamshikrishna30@gmail.com>
2026-04-23 10:49:51 -0500

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -23,7 +23,7 @@ export function renderOrderSummary(){
     'dddd, MMMM D'
     );
     cartSummaryHTML +=`
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
             Delivery date: ${dateString}
           </div>
@@ -39,7 +39,7 @@ export function renderOrderSummary(){
               <div class="product-price">
                 $${formatCurrency(matchingProduct.priceCents)}
               </div>
-              <div class="product-quantity">
+              <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                 <span>
                   Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
@@ -48,7 +48,7 @@ export function renderOrderSummary(){
                 </span>
                 <input class="quantity-input" data-product-id="${matchingProduct.id}">
                 <span class="save-quantity-link  link-primary" data-product-id="${matchingProduct.id}">Save</span>
-                <span class="delete-quantity-link link-primary js-link-delete" data-product-id="${matchingProduct.id}">
+                <span class="delete-quantity-link link-primary js-link-delete js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                   Delete
                 </span>
               </div>
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -62,6 +62,5 @@ export function renderPaymentSummary(){
   </button>
  `;
 
- document.querySelector('.js-payment-summary')
-  .innerHTML = paymentSummaryHtml;
+ document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
 }
